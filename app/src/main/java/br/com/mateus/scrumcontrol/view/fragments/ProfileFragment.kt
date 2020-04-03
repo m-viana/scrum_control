b2ff24214ca0f3671af0914d642d9d242ca00cd4
m-viana <mateusjcviana@gmail.com>
2020-04-03 18:59:59 -0300

    Finalização layout perfil e bottom sheet para opção sair

--- app/src/main/java/br/com/mateus/scrumcontrol/view/fragments/ProfileFragment.kt
+++ app/src/main/java/br/com/mateus/scrumcontrol/view/fragments/ProfileFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
 import br.com.mateus.scrumcontrol.R
 import br.com.mateus.scrumcontrol.viewmodel.ProfileViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment : Fragment() {
@@ -53,6 +56,23 @@ class ProfileFragment : Fragment() {
             val bundle = Bundle()
             bundle.putString("key_about", "TERMOS DE USO")
             findNavController().navigate(R.id.aboutFragment, bundle) }
+
+        /*Subindo o bottom sheet ao clicar em sair*/
+        tv_exit.setOnClickListener {
+            val dialog = BottomSheetDialog(context!!)
+            val view = layoutInflater.inflate(R.layout.bottom_sheet, null)
+
+            /*dando dismiss nos botoes sim e não do bottom sheet*/
+            val closeNo = view.findViewById<Button>(R.id.btn_no)
+            closeNo.setOnClickListener { dialog.dismiss() }
+
+            val closeYes = view.findViewById<Button>(R.id.btn_yes)
+            closeYes.setOnClickListener { dialog.dismiss() }
+            /*--*/
+
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 
 
